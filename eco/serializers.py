e98f8c15c8cd92067fabccbd7c73198c34212595
Luciano Ganzero <lucianoganzero94@gmail.com>
2024-11-28 11:29:15 -0300

    Consulta devuelve el nombre del tipo del material

--- eco/serializers.py
+++ eco/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Ordenes,Puntos
 
 class OrdenesSerializer(serializers.ModelSerializer):
+    tipo_nombre = serializers.ReadOnlyField(source='tipo.nombre')
     class Meta:
         model = Ordenes
         fields = '__all__'
